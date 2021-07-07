7fed49c9b9f983ad6416986661ef637459723bcb
Salome Thirot <salome.thirot@arm.com>
2021-07-07 10:24:43 +0100

    CC3XX: Hardcode entry points for the CC3XX driver
    Signed-off-by: Salome Thirot <salome.thirot@arm.com>
    Signed-off-by: Abbas Bracken Ziad <abbas.brackenziad@arm.com>
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>

--- include/psa/crypto_driver_contexts_primitives.h
+++ include/psa/crypto_driver_contexts_primitives.h
@@ -40,6 +40,9 @@
 
 /* Include the context structure definitions for those drivers that were
  * declared during the autogeneration process. */
+#if defined(PSA_CRYPTO_DRIVER_CC3XX)
+#include "cc3xx_crypto_primitives_private.h"
+#endif /* PSA_CRYPTO_DRIVER_CC3XX */
 
 #if defined(MBEDTLS_TEST_LIBTESTDRIVER1)
 #include <libtestdriver1/include/psa/crypto.h>
@@ -102,6 +105,9 @@ typedef union {
 #if defined(PSA_CRYPTO_DRIVER_TEST)
     mbedtls_transparent_test_driver_hash_operation_t test_driver_ctx;
 #endif
+#if defined(PSA_CRYPTO_DRIVER_CC3XX)
+    cc3xx_hash_operation_t cc3xx_driver_ctx;
+#endif
 } psa_driver_hash_context_t;
 
 typedef union {
@@ -110,6 +116,9 @@ typedef union {
 #if defined(PSA_CRYPTO_DRIVER_TEST)
     mbedtls_transparent_test_driver_cipher_operation_t transparent_test_driver_ctx;
     mbedtls_opaque_test_driver_cipher_operation_t opaque_test_driver_ctx;
+#endif
+#if defined(PSA_CRYPTO_DRIVER_CC3XX)
+    cc3xx_cipher_operation_t cc3xx_driver_ctx;
 #endif
 } psa_driver_cipher_context_t;
 
