c2a33affc4668fd76681d1ca456105bb441bba47
Flavio Ceolin <flavio.ceolin@intel.com>
2021-08-14 11:45:20 -0700

    mbedtls: bump mbedtls 3.0.0
    Bump mbedtls version to 3.0.0 release.
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>

--- mbedtls/include/mbedtls/net_sockets.h
+++ mbedtls/include/mbedtls/net_sockets.h
@@ -37,12 +37,9 @@
  */
 #ifndef MBEDTLS_NET_SOCKETS_H
 #define MBEDTLS_NET_SOCKETS_H
+#include "mbedtls/private_access.h"
 
-#if !defined(MBEDTLS_CONFIG_FILE)
-#include "mbedtls/config.h"
-#else
-#include MBEDTLS_CONFIG_FILE
-#endif
+#include "mbedtls/build_info.h"
 
 #include "mbedtls/ssl.h"
 
@@ -84,7 +81,7 @@ extern "C" {
  */
 typedef struct mbedtls_net_context
 {
-    int fd;             /**< The underlying file descriptor                 */
+    int MBEDTLS_PRIVATE(fd);             /**< The underlying file descriptor                 */
 }
 mbedtls_net_context;
 
