6b60f45e4df61675bd0fb60df251040bfd396d54
Antonio de Angelis <Antonio.deAngelis@arm.com>
2024-03-21 12:58:37 +0000

    Hardcode CC3XX entry points
    Manually hardcode PSA driver entry points for the CC3XX driver
    into psa crypto driver wrappers file (and provide missing entry point
    definitions if any). This is a temporary solution until the codegen
    framework is available for automatic integration.
    
    Signed-off-by: Antonio de Angelis <antonio.deangelis@arm.com>
    
    applied using:
    git am modules/tee/tf-m/trusted-firmware-m/lib/ext/mbedcrypto/\
    0005-Hardcode-CC3XX-entry-points.patch
    
    Signed-off-by: Tomi Fontanilles <tomi.fontanilles@nordicsemi.no>

--- include/psa/crypto_driver_contexts_composites.h
+++ include/psa/crypto_driver_contexts_composites.h
@@ -30,6 +30,10 @@
 /* Include the context structure definitions for those drivers that were
  * declared during the autogeneration process. */
 
+#if defined(PSA_CRYPTO_DRIVER_CC3XX)
+#include "cc3xx_crypto_primitives_private.h"
+#endif
+
 #if defined(MBEDTLS_TEST_LIBTESTDRIVER1)
 #include <libtestdriver1/include/psa/crypto.h>
 #endif
@@ -118,6 +122,9 @@ typedef union {
     mbedtls_transparent_test_driver_mac_operation_t transparent_test_driver_ctx;
     mbedtls_opaque_test_driver_mac_operation_t opaque_test_driver_ctx;
 #endif
+#if defined(PSA_CRYPTO_DRIVER_CC3XX)
+    cc3xx_mac_operation_t cc3xx_driver_ctx;
+#endif
 } psa_driver_mac_context_t;
 
 typedef union {
@@ -126,6 +133,9 @@ typedef union {
 #if defined(PSA_CRYPTO_DRIVER_TEST)
     mbedtls_transparent_test_driver_aead_operation_t transparent_test_driver_ctx;
 #endif
+#if defined(PSA_CRYPTO_DRIVER_CC3XX)
+    cc3xx_aead_operation_t cc3xx_driver_ctx;
+#endif /* PSA_CRYPTO_DRIVER_CC3XX */
 } psa_driver_aead_context_t;
 
 typedef union {
