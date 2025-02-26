3645683615fbd0e5f052322c320ab76db91b5c6a
Arif Balik <arifbalik@outlook.com>
2025-02-26 18:25:48 +0300

    fix: library: ssl_tls12_client debug message
    Fixed a debug message log level. It was printing at error level which may confuse people who do debug
    
    Signed-off-by: Arif Balik <arifbalik@outlook.com>

--- library/ssl_tls12_client.c
+++ library/ssl_tls12_client.c
@@ -2764,7 +2764,7 @@ static int ssl_write_client_key_exchange(mbedtls_ssl_context *ssl)
 
         header_len = 4;
 
-        MBEDTLS_SSL_DEBUG_MSG(1, ("Perform PSA-based ECDH computation."));
+        MBEDTLS_SSL_DEBUG_MSG(3, ("Perform PSA-based ECDH computation."));
 
         /*
          * Generate EC private key for ECDHE exchange.
